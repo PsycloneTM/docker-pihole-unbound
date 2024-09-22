07098aa9d985429fb9a234a0a52d1d5410c1f030
CycloneTM <ianbuddy278@gmail.com>
2024-09-22 21:26:34 +0000

    Update build_and_push.sh

--- one-container/pihole-unbound/build_and_push.sh
+++ one-container/pihole-unbound/build_and_push.sh
@@ -2,6 +2,6 @@
 # Run this once: docker buildx create --use --name build --node build --driver-opt network=host
 PIHOLE_VER=`cat VERSION`
 docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t cyclonetm/pihole-unbound:$PIHOLE_VER --push .
-docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t cyclonetm/pihole-unbound:latest --push .
+docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t cyclonetm/pihole-unbound:nightly --push .
 
 
